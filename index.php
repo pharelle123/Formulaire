394c31f7b52be0cc62c23bfe85d0a13e4bc7556e
pharelle123 <zeffelesra@gmail.com>
2025-05-07 12:11:53 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulaire d'Inscription</title>
+    <link rel="stylesheet" href="./css/main.css">
+</head>
+<body>
+    
+
+    <header>
+            <h1>Complèter le code PHP suivant</h1>
+    </header>
+
+    <main>
+
+
+
+    <?php
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+       
+        $admin = isset($_POST['admin']) ? 1 : 0;
+
+        print "<p>Valeur à enregistrer en base : " . $admin."</p>";
+    }
+?>
+
+
+        <section>
+                <fieldset>
+                    <legend>Remplir les champs</legend>
+                    
+                    <form action="index.php" method="post">
+                        <label for="login">Login</label>
+                        <input type="text"
+                        id="login" 
+                        name="login" 
+                        placeholder="Votre Login" 
+                        aria-required="true" 
+                        autofocus> 
+
+                        <label for="password">Mot De Passe</label>
+                        <input type="password"
+                        id="password"
+                        name="password"
+                        placeholder="Votre Mot De Passe"
+                        aria-required="true"
+                        > 
+                        
+                        <input type="checkbox"
+                        id="admin"
+                        name="admin"
+                        >  <label for="admin">Vous etes admin</label>
+                       
+                         
+
+                        <input type="submit" value="Envoyer">
+
+                    </form>
+                </fieldset>
+        </section>
+    </main>
+
+    <footer>
+        <strong>
+            &copy; - MIT - 2025</strong> 
+    </footer>
+
+
+</body>
+</html>
